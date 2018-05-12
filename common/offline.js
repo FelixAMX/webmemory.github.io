188090cbd9ac211d130b67d1214f03ff8fad5124
FelixAMX <35340933+FelixAMX@users.noreply.github.com>
2018-05-12 15:05:08 +0500

    Add files via upload

---
+++ common/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1526052487,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/scrin-sheet0.png",
+		"images/cam-sheet0.png",
+		"images/mysnapshot-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
